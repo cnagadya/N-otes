bde21eebfd0c98da552479f24992691bb5040f4c
cnagadya <ncritah@gmail.com>
2018-08-21 22:15:43 +0300

    Adding routes and functions for creating a note and getting all notes

---
+++ api/controllers/notesController.js
@@ -0,0 +1,19 @@
+"use strict";
+const mongoose = require("mongoose");
+
+Note = mongoose("Notes");
+exports.list_all_notes = function(req, res) {
+  Note.find({}, function(err, note) {
+    if (err) res.send(err);
+    res.json(note);
+  });
+};
+
+exports.create_note = function(req, res) {
+  let new_note = new Note(req.body);
+
+  new_note.save(function(err, note) {
+    if (err) res.send(err);
+    res.json(note);
+  });
+};
---
+++ api/routes/notesRoutes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = function(app) {
+  const notes = require("../controllers/notesController");
+
+  app
+    .route("/notes")
+    .get(notes.list_all_notes)
+    .post(notes.create_note);
+};
